6861ec579efe4899006cce9a7551cb4e8d7b5598
PepikEdvik <115477581+PepikEdvik@users.noreply.github.com>
2024-12-19 21:58:59 +0100

    Add files via upload

---
+++ maturita/maturita PVA/kvíz/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Jaký je hlavní účel elektronické cigarety?",
+        answer: "A. Nahrazovat tradiční cigarety, zejména při odvykání kouření",
+        options: [
+            "A. Nahrazovat tradiční cigarety, zejména při odvykání kouření",
+            "B. Snižovat množství vdechovaného kouře",
+            "C. Zlepšovat kvalitu vzduchu",
+            "D. Pomáhat lidem při hubnutí"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Co je hlavní složkou náplně elektronických cigaret?",
+        answer: "C. Propylenglykol a glycerin",
+        options: [
+            "A. Vodík",
+            "B. Kyslík",
+            "C. Propylenglykol a glycerin",
+            "D. Ethanol"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Jaký zdravotní problém je často spojován s dlouhodobým používáním elektronických cigaret?",
+        answer: "B. Zhoršení zraku",
+        options: [
+            "A. Ztráta sluchu",
+            "B. Zhoršení zraku",
+            "C. Oslabení kostí",
+            "D. Poškození tkáně"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Jaký je rozdíl mezi klasickými cigaretami a elektronickými cigaretami z hlediska produkce škodlivých látek?",
+        answer: "D. Elektronické cigarety produkují méně karcinogenů než klasické cigarety",
+        options: [
+            "A. Elektronické cigarety produkují více karcinogenů než klasické cigarety",
+            "B. Elektronické cigarety produkují stené množství karcinogenůjako normální cigarety",
+            "C. Elektronické cigarety neprodukují žádné škodlivé látky",
+            "D. Elektronické cigarety produkují méně karcinogenů než klasické cigarety",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Která z následujících látek může být návyková a je obvykle přítomna v elektronických cigaretách?",
+        answer: "B. Nikotin",
+        options: [
+            "A. Sůl",
+            "B. Nikotin",
+            "C. Oxid uhličitý",
+            "D. Vitamin C"
+        ]
+    }
+];
